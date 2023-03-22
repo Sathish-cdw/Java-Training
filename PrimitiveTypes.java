f30ba11a637474be6ef79e2ac1429ed778eb85d4
Sathish-cdw <125631837+Sathish-cdw@users.noreply.github.com>
2023-03-22 18:30:29 +0530

    Update PrimitiveTypes.java

--- PrimitiveTypes.java
+++ PrimitiveTypes.java
@@ -2,23 +2,23 @@ package task1;
 
 public class PrimitiveTypes 
 {
-	static boolean val1;
-	   static double val2;
-	   static float val3;
-	   static int val4;
-	   static long val5;
-	   static String val6;
-	   static char val7;
-	   static short val8;
+	static boolean booleanValue;
+	   static double doubleValue;
+	   static float floatValue;
+	   static int intValue;
+	   static long longValue;
+	   static String stringValue;
+	   static char charValue;
+	   static short shortValue;
 	   public static void main(String[] args)
 	   {
-	      System.out.println("Val1 = " + val1);
-	      System.out.println("Val2 = " + val2);
-	      System.out.println("Val3 = " + val3);
-	      System.out.println("Val4 = " + val4);
-	      System.out.println("Val5 = " + val5);
-	      System.out.println("Val6 = " + val6);
-	      System.out.println("Val7 = " + val7);
-	      System.out.println("Val8 = " + val8);
+	      System.out.println("Boolean Value = " + booleanValue);
+	      System.out.println("Double Value = " + doubleValue);
+	      System.out.println("Float Value = " + floatValue);
+	      System.out.println("Int Value = " + intValue);
+	      System.out.println("Long Value = " + longValue);
+	      System.out.println("String Value = " + StringValue);
+	      System.out.println("Char Value = " + charValue);
+	      System.out.println("Short Value = " + shortValue);
 	   }
 }
