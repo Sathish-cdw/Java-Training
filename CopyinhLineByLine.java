2fda7e91c186cc0c323df45ee5f98afddb0ca6a3
Sathish-cdw <125631837+Sathish-cdw@users.noreply.github.com>
2023-03-31 12:11:23 +0530

    Update CopyinhLineByLine.java

--- CopyinhLineByLine.java
+++ CopyinhLineByLine.java
@@ -9,22 +9,16 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-
-
-public class CopyinhLineByLine {
+public class CopyLineByLine {
     public static void main(String[] args) throws IOException {
-
         BufferedReader inputStream = null;
         PrintWriter outputStream = null;
-
         try {
             inputStream = new BufferedReader(new FileReader("data2.txt"));
-            outputStream = new PrintWriter(new FileWriter(
-                    "characteroutput.txt"));
-
-            String l;
-            while ((l = inputStream.readLine()) != null) {
-                outputStream.println(l);
+            outputStream = new PrintWriter(new FileWriter("characteroutput.txt"));
+            String s;
+            while ((s= inputStream.readLine()) != null) {
+                outputStream.println(s);
             }
         } finally {
             if (inputStream != null) {
