daf8cea8508a51558442fa3b922c80784af5e162
Sathish-cdw <125631837+Sathish-cdw@users.noreply.github.com>
2023-03-31 13:38:40 +0530

    Update CreateTable.java

--- CreateTable.java
+++ CreateTable.java
@@ -13,15 +13,16 @@ public class CreateTable {
    public static void main(String[] args) {
       try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
          Statement stmt = conn.createStatement();
-      ) {		      
-          String sql = "CREATE TABLE REGISTRATION " +
+      ) {		
+         //query to create table
+          String query = "CREATE TABLE REGISTRATION " +
                    "(id INTEGER not NULL, " +
                    " first VARCHAR(255), " + 
                    " last VARCHAR(255), " + 
                    " age INTEGER, " + 
                    " PRIMARY KEY ( id ))"; 
 
-         stmt.executeUpdate(sql);
+         stmt.executeUpdate(query);
          System.out.println("Created table in given database...");   	  
       } catch (SQLException e) {
          e.printStackTrace();
