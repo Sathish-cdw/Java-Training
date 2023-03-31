913a54036151cb02b43d1bebcb8145ea430d1142
Sathish-cdw <125631837+Sathish-cdw@users.noreply.github.com>
2023-03-31 12:35:44 +0530

    Update StdIn.java

--- StdIn.java
+++ StdIn.java
@@ -4,16 +4,20 @@ import java.util.Scanner;
 
 public class StdIn {
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
+        try (Scanner scanner = new Scanner(System.in)) {
             int characterCount = 0;
             System.out.println("Enter words or Press Ctrl+D to exit: ");
-            while(sc.hasNextLine()) {
-                String line = sc.nextLine();
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine();
                 String[] words = line.split(" ");
                 for(int i=0;i<words.length;i++)
                     characterCount += words[i].length();
             }
             System.out.println("Number of words: "+characterCount);
+        }
+        catch(Exception e)
+        {
+        	e.printStackTrace();
         }
     }
 }
