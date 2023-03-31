e905e9c3ab09adb5f389ad866d6b50addddd1ad6
Sathish-cdw <125631837+Sathish-cdw@users.noreply.github.com>
2023-03-31 12:33:26 +0530

    Update PrintContent.java

--- PrintContent.java
+++ PrintContent.java
@@ -7,26 +7,22 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class PrintContent {
-
-public static void main(String[] args) {
-	
-
-File file = new File("Read.txt");
-
-if (file.exists()) {
-	try (BufferedReader br = new BufferedReader(new FileReader("Read.txt"))) {
-		   String line = null;
-		   while ((line = br.readLine()) != null) {
-		       System.out.println(line);
-		   }
-		}
-	catch (IOException e) {
-        e.printStackTrace();
-    }
-}
-else {
-    System.out.println("file doesn't exist");
-}
-
-}
+	public static void main(String[] args) {
+		File fileRead = new File("Read.txt");
+			//if file exist
+			if (fileRead.exists()) {
+				try (BufferedReader br = new BufferedReader(new FileReader("Read.txt"))) {
+		   			String line = null;
+		  			while ((line = br.readLine()) != null) {
+		       				System.out.println(line);
+		   			}
+				}
+				catch (IOException e) {
+        				e.printStackTrace();
+    				}
+			}
+			else {
+    				System.out.println("file doesn't exist");
+			}
+	}
 }
